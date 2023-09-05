039e807e8128bdd857ecb165d1b4d1d8f8e8be8a
StanDobrev11 <dobrev81@gmail.com>
2023-09-05 04:27:47 +0000

    Update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from tkinter.ttk import *
 from tkinter import *
-
+from tkcalendar import DateEntry
 
 def clear_view():
     for slave in tk.grid_slaves():
@@ -19,8 +20,18 @@ def render_settings_window():
     set_win.geometry('500x500')
     set_win.title('Settings')
     Label(set_win, text='Enter computer name: ').grid(row=0, column=0)
+    Entry(set_win).grid(row=0, column=1)
+    Label(set_win, text='DUE DATE: ').grid(row=1, column=0)
+    DateEntry(set_win).grid(row=1, column=1)
+
+
+    Button(set_win, text="OK").grid(row=10, column=0)
+    Button(set_win, text="Cancel").grid(row=10, column=1)
+
     tk.withdraw()
-    
+
+    # tk.deiconify()
+
 
 tk = Tk()
 tk.geometry('250x300')
